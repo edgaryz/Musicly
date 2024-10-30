032bb4b5f2a592d4c2f4d7826d20b42c1b6764ff
Edgar Sokol <edgar.sokol@gmail.com>
2024-10-30 21:56:29 +0200

    upgrade

--- src/main/java/com/example/musicProject/controllers/MusicController.java
+++ src/main/java/com/example/musicProject/controllers/MusicController.java
@@ -1,24 +1,28 @@
 package com.example.musicProject.controllers;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
 @RequestMapping("/api")
 public class MusicController {
     @RestController
     @RequestMapping("/api")
-    public class SongController {
+    public static class SongController {
+        @GetMapping("/song/{filename}")
+        public ResponseEntity<Resource> getSong(@PathVariable String filename) {
 
-        @GetMapping("/song")
-        public ResponseEntity<Resource> getSong() {
-            Resource resource = new ClassPathResource("static/song.mp3");
+            Path currentPath = Paths.get(System.getProperty("user.dir"));
+            Path filePath = Paths.get(currentPath.toString(), "song", filename);
+            Resource resource = new FileSystemResource(filePath);
+            System.out.println();
             return ResponseEntity.ok()
                     .contentType(MediaType.parseMediaType("audio/mpeg"))
                     .body(resource);
